ab8c672a4df6c92e39e594e5f2d95b05bf786d59
dprimorac21 <dino.primorac2@gmail.com>
2025-08-13 06:16:30 +0200

    Postavljanje testne okoline i skripte za upravljanje inputima igrača

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [Header("Input Actions:")]
+    [SerializeField] private InputActionAsset playerControls = null;
+    [Header("Action Map")]
+    [SerializeField] private string actionMapName = "PlayerActionMap";
+    [Header("Action Name References")]
+    [SerializeField] private string movement = "Movement";
+    [SerializeField] private string rotation = "Rotation";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string primaryFire = "PrimaryFire";
+
+    private InputAction movementAction = null;
+    private InputAction rotationAction = null;
+    private InputAction jumpAction = null;
+    private InputAction primaryFireAction = null;
+
+    public Vector2 MovementInput = Vector2.zero;
+    public Vector2 RotationInput = Vector2.zero;
+    public bool JumpTriggered = false;
+    public bool FirePrimaryTriggered = false;
+
+    private void Awake()
+    {
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+        movementAction = mapReference.FindAction(movement);
+        rotationAction = mapReference.FindAction(rotation);
+        jumpAction = mapReference.FindAction(jump);
+        primaryFireAction = mapReference.FindAction(primaryFire);
+        SubscribeActionsToEvents();
+    }
+    
+    private void SubscribeActionsToEvents()
+    {
+        movementAction.performed += ctx => MovementInput = ctx.ReadValue<Vector2>();
+        movementAction.canceled += ctx => MovementInput = Vector2.zero;
+        rotationAction.performed += ctx => RotationInput = ctx.ReadValue<Vector2>();
+        rotationAction.canceled += ctx => RotationInput = Vector2.zero;
+        jumpAction.performed += ctx => JumpTriggered = true;
+        jumpAction.canceled += ctx => JumpTriggered = false;
+        primaryFireAction.performed += ctx => FirePrimaryTriggered = true;
+        primaryFireAction.canceled += ctx => FirePrimaryTriggered = false;
+
+    }
+    private void OnEnable()
+    {
+        playerControls.FindActionMap(actionMapName).Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.FindActionMap(actionMapName).Disable();
+    }
+
+}
